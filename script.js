06c5651e3d8a74741193042f082c44f0000bde04
ainroman <romaa622@newschool.edu>
2025-02-16 05:02:05 +0000

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function paintImage(e){
-    document.querySelector('#heart').innerHTML += `<img src="media/heart.png" style="left:${e.offsetX}px;top:${e.offsetY}px; z-index: 5">`;
+    document.querySelector('#heart').innerHTML += `<img src="media/Heart.png" style="left:${e.offsetX}px;top:${e.offsetY}px; z-index: 5">`;
    }
 document.addEventListener('click', paintImage);
 
